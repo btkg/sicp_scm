dae1b26f4fb1b4e663218f59318967011f3b09d5
btkg <zy119159@gmail.com>
2021-09-21 21:30:26 +0800

    tmp

--- ch01/1_1.scm
+++ ch01/1_1.scm
@@ -3,9 +3,12 @@
         (< (abs (- (square guess) x)) 0.001))
     (define (improve guess)
         (average guess (/ x guess)))
+    (define (average x y)
+        (/ (+ x y) 2))
     (define (sqrt-iter guess)
         (if (good-enough? guess)
             guess
             (sqrt-iter (improve guess))))
     (sqrt-iter 1.0)
 )
+(sqrt 9)
---
+++ ch02/cons.scm
@@ -0,0 +1,7 @@
+(define x (cons 1 2))
+(car x)
+(cdr x)
+(define y (cons 3 4))
+(define z (cons x y))
+(car (car z))
+(car (cdr z))
